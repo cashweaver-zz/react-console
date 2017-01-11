7efd6ce7e10eb369f1ee052ffc14a9210f6d464c
cashweaver <cashbweaver@gmail.com>
2017-01-11 12:11:59 -0800

    MVP

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Console from './../Console';
 import './App.css';
 
-const history = [
-  {command: 'ls', output: 'a_file.txt'},
-  {command: 'cat a_file.txt', output: 'The contents of the file.'},
-];
 
-const promptPrefix = '$ ';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => (
-  <div className="app">
-    <Console
-      history={history}
-      promptPrefix={promptPrefix}
-    />
-  </div>
-);
+    this.state = {
+      promptPrefix: '$ ',
+      history: {
+        commands: [],
+        results: [],
+      },
+    }
+
+    this.runCommand = this.runCommand.bind(this);
+  }
+
+  runCommand(command) {
+    const result = `result of running ${command}`;
+    console.log('runCommand', this.state.history);
+    this.setState({
+      history: {
+        commands: [command].concat(this.state.history.commands),
+        results: [result].concat(this.state.history.results),
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Console
+          history={this.state.history}
+          promptPrefix={this.state.promptPrefix}
+          runCommand={this.runCommand}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Console/index.js
+++ src/components/Console/index.js
@@ -1,25 +1,143 @@
 import React, { Component } from 'react';
+import History from './../History';
 import './Console.css';
 
 class Console extends Component {
+  constructor(props) {
+    super(props);
+
+    this.keyCodes = {
+      modifiers: [
+        'AltRight',
+        'AltLeft',
+        'ControlRight',
+        'ControlLeft',
+      ],
+      arrows: [
+        'ArrowUp',
+        'ArrowDown',
+        'ArrowLeft',
+        'ArrowRight',
+      ],
+    }
+
+    this.handleKeydown = this.handleKeydown.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.handleBackspace = this.handleBackspace.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    let initialCommand = '';
+    if ('command' in props) {
+      initialCommand = props.command;
+    }
+
+    this.state = {
+      commands: [initialCommand].concat(props.history.commands),
+      commandIndex: 0,
+    }
+  }
+
+  componentDidMount() {
+    this.consoleElement.addEventListener('keydown', this.handleKeydown);
+    this.consoleElement.addEventListener('click', this.handleClick);
+  }
+
+  componentWillUnmount() {
+    this.consoleElement.removeEventListener('keydown', this.handleKeydown);
+    this.consoleElement.removeEventListener('click', this.handleClick);
+  }
+
+  componentWillUpdate() {
+    this.scrollWithNewInput();
+  }
+
+  scrollWithNewInput() {
+    this.consoleElement.scrollTop = this.consoleElement.scrollHeight;
+  }
+
+  handleClick(e) {
+    this.consoleElement.focus();
+  }
+
+  handleKeydown(e) {
+    if (e.code === 'Enter') {
+      this.handleSubmit();
+    } else if (e.code === 'Backspace') {
+      this.handleBackspace();
+    } else if (this.keyCodes.modifiers.includes(e.code)) {
+      this.handleModifier(e);
+    } else if (this.keyCodes.arrows.includes(e.code)) {
+      this.handleArrow(e);
+    } else {
+      const newCommands = this.state.commands;
+      newCommands[this.state.commandIndex] += e.key;
+      this.setState({
+        commands: newCommands,
+      });
+    }
+  }
+
+  handleArrow(e) {
+    if (e.code === 'ArrowLeft') {
+      // TODO
+    } else if (e.code === 'ArrowRight') {
+      // TODO
+    } else if (e.code === 'ArrowUp') {
+      if (this.state.commandIndex < this.state.commands.length - 1) {
+        this.setState({
+          commandIndex: this.state.commandIndex + 1,
+        })
+      }
+    } else if (e.code === 'ArrowDown') {
+      if (this.state.commandIndex > 0) {
+        this.setState({
+          commandIndex: this.state.commandIndex - 1,
+        })
+      }
+    }
+  }
+
+  handleModifier() {
+  }
+
+  handleBackspace() {
+    const newCommands = this.state.commands;
+    const curCommand = newCommands[this.state.commandIndex];
+    newCommands[this.state.commandIndex] = curCommand.substring(0, curCommand.length - 1);
+    this.setState({
+      commands: newCommands,
+    });
+  }
+
+  handleSubmit() {
+    this.props.runCommand(this.state.commands[this.state.commandIndex]);
+    this.setState({
+      commands: [''].concat(this.state.commands)
+    });
+
+    this.setState({
+      commandIndex: 0,
+    });
+  }
+
   render() {
     return (
-      <div className="console">
-        <h1>Console</h1>
-        <div className="history">
-          <ul>
-            {this.props.history.map((item, i) => (
-              <div className="history-item" key={i}>
-                <li>{this.props.promptPrefix}{item.command}</li>
-                <li>{item.output}</li>
-              </div>
-            ))}
-          </ul>
-        </div>
-        <div className="prompt">
-          <span className="prefix">{this.props.promptPrefix}</span>
-          <span className="command"></span>
-        </div>
+      <div
+        ref={(el) => this.consoleElement = el}
+        className="console"
+        tabIndex="1"
+      >
+        <ul>
+          <History
+            {...this.props.history}
+            promptPrefix={this.props.promptPrefix}
+          />
+          <li className="prompt">
+            <span className="prefix">{this.props.promptPrefix}</span>
+            <span className="command">{this.state.commands[this.state.commandIndex]}</span>
+          </li>
+        </ul>
       </div>
     );
   }
